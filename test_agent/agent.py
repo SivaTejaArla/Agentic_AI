8731c37e3afb1c89e2f619cd30870662c06d7418
Siva Teja Arla <asivateja@collaborativesolutions.com>
2026-08-16 18:00:52 +0530

    Comments added to test_agent

--- test_agent/agent.py
+++ test_agent/agent.py
@@ -1,10 +1,30 @@
 from google.adk.agents.llm_agent import Agent
 
+
+from datetime import datetime
+
+
+
+def getCurrentTime():
+
+    currentTime = datetime.now()
+
+    return currentTime
+def convertToDays(age: int) -> int:
+    return age*365
+
 root_agent = Agent(
-    model='gemini-flash-latest',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Greet the user first based on the current time of user and Answer user questions to the best of your knowledge',
+    model="gemini-flash-latest",  ## Name of the model being used 
+    name="root_agent",              ## Name of the agent 
+    description="A helpful assistant for user questions.", ## Description about the agent 
+    instruction="""            
+    Always start the very first response with a warm greeting based on getCurrentTime.
+    Then ask the user for their name.
+    After that,ask about their age. Convert to days using convertToDays
+
+    """, ## Instructions to the agent like how to start the conversation
+
+    tools=[getCurrentTime,convertToDays] ## External tools that can be used by the agent 
 )
 
-print("Agent Created Successfully !!  ")
+print("Agent Created Successfully!!")
